3fe55b3186c21d00ce5038999855b5ab55d75229
Jared Bruni <jaredbruni@gmail.com>
2019-05-06 14:15:09 -0700

    updated code

--- scripts/random_pixels.py
+++ scripts/random_pixels.py
@@ -1,8 +1,3 @@
-#def sum(left: int, right: int) -> int:
-#	result = left + right;
-#	print(left, ' + ', right, ' = ', result);
-#	return result;
-
 import random
 
 def filter(data: bytes) -> bytes: 
@@ -10,4 +5,5 @@ def filter(data: bytes) -> bytes:
 	for i in range(0, len(data) -1):
 		mutable_bytes[i] = random.randint(0, 255)
 	return bytes(mutable_bytes)
+
 
