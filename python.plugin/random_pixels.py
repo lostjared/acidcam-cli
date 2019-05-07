f372f37394bbe410aea99f6c9ed7f5774fcfc3a9
Jared Bruni <jaredbruni@gmail.com>
2019-05-07 09:23:44 -0700

    remove dir

--- python.plugin/random_pixels.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def filter(data: bytes) -> bytes: 
-	mutable_bytes = bytearray(data)
-	for i in range(0, len(data) -1):
-		mutable_bytes[i] = random.randint(0, 255)
-	return bytes(mutable_bytes)
-
-
