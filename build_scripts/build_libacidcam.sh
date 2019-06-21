3240c3c6de33c808b7c3a9794f969beb6312ad12
Jared Bruni <jaredbruni@gmail.com>
2019-06-21 06:05:54 -0700

    updated script

--- build_scripts/build_libacidcam.sh
+++ build_scripts/build_libacidcam.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 
 sudo apt-get install -y libopencv-dev cmake git pkg-config libavcodec-dev libavformat-dev libswscale-dev python3-dev python-numpy libjpeg-dev libpng-dev libtiff-dev autoconf automake libtool wget nano
 
